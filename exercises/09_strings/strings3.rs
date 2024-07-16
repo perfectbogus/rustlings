b62930048796c5909645f7cac01fa720ee3a6e29
Jose Escobar <jose.escobar@oracle.com>
2024-07-15 21:57:42 -0600

    String: Finished

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,35 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut low = 0;
+    for (index, char) in input.chars().enumerate() {
+        if char != ' ' {
+            low = index;
+            break
+        }
+    }
+
+    let mut up = 0;
+    for (index , char) in input.chars().rev().enumerate() {
+        if char != ' ' {
+            up = input.len() - index;
+            break
+        }
+    }
+
+    input[low..up].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    str::replace(input, "cars", "balloons")
 }
 
 #[cfg(test)]
